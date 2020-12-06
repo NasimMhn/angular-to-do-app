182ee172aea9d8b17436be7d44f4f55c5bf02cba
nasimmhn <nasim.mahzoun@gmail.com>
2020-12-06 20:54:55 +0100

    create to-do-item and to-do-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToDoItemComponent } from './to-do-item/to-do-item.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoItemComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule
