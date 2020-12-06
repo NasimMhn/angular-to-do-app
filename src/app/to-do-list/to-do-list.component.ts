c3e3b1a0ae5aebd1a2ee411d3dc52ad692a843a5
nasimmhn <nasim.mahzoun@gmail.com>
2020-12-06 22:18:59 +0100

    add class, etc

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+interface ToDoItem {
+  title: string;
+  description: string;
+  created: string;
+}
+
+class ToDoItem {
+  constructor(title: string, description: string) {
+    this.title = title;
+    this.description = description;
+    this.created = moment(new Date()).format('lll');
+  }
+}
+
 
 @Component({
   selector: 'app-to-do-list',
@@ -7,9 +23,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
 
+  pageTitle = 'To-Do List';
+
+  items: ToDoItem[] = [
+    new ToDoItem('First', 'Description 1'),
+    new ToDoItem('Second', 'Description 2'),
+    new ToDoItem('Third', 'Description 3')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
